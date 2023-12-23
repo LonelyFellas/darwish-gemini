1e04b841fb414ad4137d319f0377b7ef8fdfda10
worksapce <289582304@qq.com>
2023-12-23 19:40:23 +0800

    feat: done home page

---
+++ app/homepage/page.tsx
@@ -0,0 +1,6 @@
+export default function homepage() {
+    return <main className="flex">
+       <div className="">left</div>
+        <div className="">right</div>
+    </main>
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import Link  from "next/Link"
 export default function Home() {
   return (
-      <main className="flex justify-center items-center min-h-screen">
-        <h1 className="xl: gradient-text text-9xl">
-          Welcome to Darwish-Gemini
-        </h1>
-        {/*<Link href="./login">Go to Login in</Link>*/}
+      <main className="flex justify-center items-center min-h-screen ">
+          <Link href="homepage" className="w-[60%] text-center -mt-40 cursor-pointer" >
+              <h1 className="gradient-text text-[6vw] leading-[6vw]">
+                  Welcome to Darwish-Gemini
+              </h1>
+          </Link>
       </main>
   )
 }
