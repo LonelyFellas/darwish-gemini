4e16aae0c52d9ee7c8691c5518fefdd1cc37162f
worksapce <289582304@qq.com>
2023-12-24 11:51:46 +0800

    refactor: refactor project struct

--- hooks/useThemeMode.ts
+++
@@ -1,3 +0,0 @@
-export default function useThemeModal() {
-    return {}
-}
--- web/darwish-gemini-font/app/homepage/page.tsx
+++ web/darwish-gemini-font/app/homepage/page.tsx
@@ -1,8 +1,8 @@
 export default function homepage() {
     return (
-        <main className="relative z-0 flex h-screen w-full overflow-hidden md:flex-row sm:flex-col">
-            <div className="h-[200px]">前端</div>
-            <div className="dark flex-shrink-0 overflow-x-hidden bg-black0 h-full bg-red-400">
+        <main className="relative z-0 flex h-screen w-full overflow-hidden flex-col md:flex-row">
+            <div className="h-[200px] sm:block md:hidden">前端</div>
+            <div className="dark flex-shrink-0 overflow-x-hidden bg-black0 h-full bg-red-400 sm:hidden md:block">
                 <div className="h-full w-[260px]">
                     <div className="flex h-full min-h-0 flex-col">
                         <div className="scrollbar-trigger relative h-full w-full flex-1 items-start border-white/20">
